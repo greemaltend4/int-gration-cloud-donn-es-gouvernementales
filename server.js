1156af7aff3160352c2d477b75c6eeb6653e3d45
greemaltend4 <dengestabiy@hotmail.com>
2025-10-31 09:18:46 +0000

    Changed MongoDB connection string to use environment variable MONGODB_URI if available.

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 // Connection to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/donnees?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/donnees?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connecté à MongoDB'))
   .catch(err => console.error('Erreur de connexion à MongoDB', err));
 
