60607df66c8b0a2a282b335849e971ef0c4d05a8
greemaltend4 <dengestabiy@hotmail.com>
2025-10-31 09:15:19 +0000

    feat: initial commit for frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Intégration Cloud des Données Gouvernementales</h1>
+        <p>Bienvenue dans le portail des données ouvertes.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
